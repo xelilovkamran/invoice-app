3dd6e5b07800811bbd6fd294dbe26027826de8ac
xelilovkamran <xlilovkamran@gmail.com>
2024-06-18 22:52:42 +0400

    added background images for signin and signup pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: "class",
     theme: {
-        extend: {},
-        screens: {
-            tablet: "860px",
-            mobile: "550px",
+        extend: {
+            backgroundImage: {
+                "signin-background": "url('/src/assets/signin-background.png')",
+                "signup-background": "url('/src/assets/signup-background.png')",
+            },
+            screens: {
+                tablet: "860px",
+                mobile: "550px",
+            },
         },
     },
 };
